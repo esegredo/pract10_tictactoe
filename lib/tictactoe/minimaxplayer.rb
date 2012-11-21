450a0d695f171a2aa8f9d5ac2f11e1b209593e57
Eduardo Segredo <edusegre@gmail.com>
2012-11-21 13:29:04 +0000

    Some bugs in minimax player have been solved

--- lib/tictactoe/minimaxplayer.rb
+++ lib/tictactoe/minimaxplayer.rb
@@ -10,14 +10,51 @@ module Tictactoe
       bestValue = -2
       bestMove = ""
       moves.each do |move|
-        newboard = Board.new(board.squares)
+        newsquares = board.squares.dup
+        newboard = Board.new(newsquares)
         value = search_best_move(newboard, move, self.mark, 1)
-        if (value > bestValue)
+        if (value >= bestValue)
           bestValue = value
           bestMove = move
         end
       end
       bestMove
+    end
+
+    def search_best_move(board, move, mark, level)
+      # Puts the movement on the board
+      board[move] = mark
+      moves = board.moves
+
+      # Checks whether there is a winner or not
+      return 1 if (board.won? == self.mark)
+      return 0 if (board.won? == " ")
+      return -1 if (board.won? == opponent_mark)
+
+      # Max level
+      if ((level % 2 == 1) && (!board.won?))
+        bestValue = -2;
+        moves.each do |mov|
+          newsquares = board.squares.dup
+          newboard = Board.new(newsquares)
+          value = search_best_move(newboard, mov, opponent_mark, level + 1)
+          if (value >= bestValue)
+            bestValue = value
+          end
+        end
+      # Min level
+      elsif ((level % 2 == 0) && (!board.won?))
+        bestValue = 2;
+        moves.each do |mov|
+          newsquares = board.squares.dup
+          newboard = Board.new(newsquares)
+          value = search_best_move(newboard, mov, self.mark, level + 1)
+          if (value <= bestValue)
+            bestValue = value
+          end
+        end
+      end
+      bestValue
     end
 
     def opponent_mark
@@ -28,45 +65,5 @@ module Tictactoe
       end
     end
 
-    def search_best_move(board, move, mark, level)
-
-      # Puts the movement on the board
-      board[move] = mark
-      moves = board.moves
-
-      # Max level
-      if (level % 2 == 1)
-        return 1 if (board.won? == mark)
-        return 0 if (board.won? == " ")
-        return -1 if (board.won? == opponent_mark)
-        if (!board.won?)
-          bestValue = -2;
-          moves.each do |mov|
-            newboard = Board.new(board.squares)
-            value = search_best_move(newboard, mov, opponent_mark, level + 1)
-            if (value > bestValue)
-              bestValue = value
-            end
-          end
-          return bestValue
-        end
-      # Min level
-      elsif (level % 2 == 0)
-        return -1 if (board.won? == opponent_mark)
-        return 0 if (board.won? == " ")
-        return 1 if (board.won? == self.mark)
-        if (!board.won?)
-          bestValue = 2;
-          moves.each do |mov|
-            newboard = Board.new(board.squares)
-            value = search_best_move(newboard, mov, self.mark, level + 1)
-            if (value < bestValue)
-              bestValue = value
-            end
-          end
-          return bestValue
-        end
-      end
-    end
   end
 end
